5d26afdfd4ec6f1ed1066332c8e7e024dcd75e43
Luma <world@luma.email>
2021-09-11 00:34:08 +0900

    fix deps trailing chars

--- denops/ddc-tabnine/deps.ts
+++ denops/ddc-tabnine/deps.ts
@@ -1,10 +1,10 @@
-export type { Candidate } from "https://deno.land/x/ddc_vim@v0.3.0/types.ts#^";
-export { BaseSource } from "https://deno.land/x/ddc_vim@v0.3.0/types.ts#^";
-export type { Denops } from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts#^";
-export { fn, vars } from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts#^";
+export type { Candidate } from "https://deno.land/x/ddc_vim@v0.3.0/types.ts";
+export { BaseSource } from "https://deno.land/x/ddc_vim@v0.3.0/types.ts";
+export type { Denops } from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts";
+export { fn, vars } from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts";
 export type {
   GatherCandidatesArguments,
-} from "https://deno.land/x/ddc_vim@v0.3.0/base/source.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.3.0/base/source.ts";
 export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
 export * as io from "https://deno.land/std@0.106.0/io/mod.ts";
 export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
