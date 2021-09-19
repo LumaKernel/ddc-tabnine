7b720d640187a57ce47fe9b44d4677e6a92b86dd
Luma <world@luma.email>
2021-09-19 21:32:02 +0900

    update deno deps

--- denops/ddc-tabnine/deps.ts
+++ denops/ddc-tabnine/deps.ts
@@ -1,11 +1,11 @@
-export type { Candidate } from "https://deno.land/x/ddc_vim@v0.11.0/types.ts";
-export { BaseSource } from "https://deno.land/x/ddc_vim@v0.11.0/types.ts";
-export type { Denops } from "https://deno.land/x/ddc_vim@v0.11.0/deps.ts";
-export { fn, vars } from "https://deno.land/x/ddc_vim@v0.11.0/deps.ts";
+export type { Candidate } from "https://deno.land/x/ddc_vim@v0.12.0/types.ts";
+export { BaseSource } from "https://deno.land/x/ddc_vim@v0.12.0/types.ts";
+export type { Denops } from "https://deno.land/x/ddc_vim@v0.12.0/deps.ts";
+export { fn, vars } from "https://deno.land/x/ddc_vim@v0.12.0/deps.ts";
 export type {
   GatherCandidatesArguments,
   OnCompleteDoneArguments,
-} from "https://deno.land/x/ddc_vim@v0.11.0/base/source.ts";
+} from "https://deno.land/x/ddc_vim@v0.12.0/base/source.ts";
 export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
 export * as io from "https://deno.land/std@0.107.0/io/mod.ts";
 export * as fs from "https://deno.land/std@0.107.0/fs/mod.ts";
